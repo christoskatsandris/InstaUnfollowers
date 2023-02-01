e0d4f26fb2d0dd068833557d87aa6e5e79d94c96
Christos Katsandris <christoskatsandris@outlook.com>
2023-02-02 00:31:18 +0200

    XPath updates and minor UI tweaks

--- unfollowers.py
+++ unfollowers.py
@@ -11,21 +11,21 @@ driver.get("https://www.instagram.com/")
 
 time.sleep(2)
 
-button = driver.find_element(By.XPATH, "/html/body/div[1]/div/div/div/div[2]/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/button[2]")
+button = driver.find_element(By.XPATH, "/html/body/div[2]/div/div/div/div[2]/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/button[2]")
 button.click()
 
 time.sleep(3)
 
-username = driver.find_element(By.XPATH, "/html/body/div[1]/div/div/div/div[1]/div/div/div/div[1]/section/main/article/div[2]/div[1]/div[2]/form/div/div[1]/div/label/input")
+username = driver.find_element(By.XPATH, "/html/body/div[2]/div/div/div/div[1]/div/div/div/div[1]/section/main/article/div[2]/div[1]/div[2]/form/div/div[1]/div/label/input")
 username.send_keys(usernameinput)
 
-password = driver.find_element(By.XPATH, "/html/body/div[1]/div/div/div/div[1]/div/div/div/div[1]/section/main/article/div[2]/div[1]/div[2]/form/div/div[2]/div/label/input")
+password = driver.find_element(By.XPATH, "/html/body/div[2]/div/div/div/div[1]/div/div/div/div[1]/section/main/article/div[2]/div[1]/div[2]/form/div/div[2]/div/label/input")
 password.send_keys(passwordinput)
 
-login = driver.find_element(By.XPATH, "/html/body/div[1]/div/div/div/div[1]/div/div/div/div[1]/section/main/article/div[2]/div[1]/div[2]/form/div/div[3]/button")
+login = driver.find_element(By.XPATH, "/html/body/div[2]/div/div/div/div[1]/div/div/div/div[1]/section/main/article/div[2]/div[1]/div[2]/form/div/div[3]/button")
 login.click()
 
-time.sleep(6)
+time.sleep(10)
 
 driver.get("https://www.instagram.com/christoskatsandris/followers/")
 input("Scroll down in your browser, until all your followers are loaded. Press ENTER to continue when you're ready.")
@@ -44,6 +44,8 @@ for follower in followers:
 driver.get("https://www.instagram.com/christoskatsandris/following/")
 input("Scroll down in your browser, until all people you're following are loaded. Press ENTER to continue when you're ready.")
 
+print("Analyzing followers. Please wait...")
+
 following = driver.find_elements(By.XPATH, "//div[@class=' _ab8y  _ab94 _ab97 _ab9f _ab9k _ab9p _abcm']")
 
 following_names = []
@@ -55,6 +57,8 @@ for user in following:
     else:
         following_names.append(name)
 
+driver.quit()
+
 for follower in followers_names:
     if follower not in following_names:
         followers_that_i_dont_follow_back.append(follower)
@@ -64,3 +68,5 @@ for user in following_names:
 
 print("I don't follow back:", followers_that_i_dont_follow_back)
 print("Don't follow me back:", following_that_they_dont_follow_me_back)
+
+input("Press any key to exit.")
